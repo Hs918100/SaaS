235c52e44ae3f181ef9843f86607cf24cf1b6970
harishsharmacoupa <harish.sharma@coupa.com>
2025-02-07 21:08:11 +0530

    added devise authenitcation and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -8,4 +9,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+# log out won't work without this
+devise_scope :user do  
+  get '/users/sign_out' => 'devise/sessions#destroy'     
+end
+
 end
